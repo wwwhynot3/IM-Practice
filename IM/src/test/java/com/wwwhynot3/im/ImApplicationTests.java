4aac924e0da4399058b8eeef15fb128f429a8f4f
Huang Lingbo <wwwhynot3@foxmail.com>
2024-01-31 20:13:38 +0800

    IM init

---
+++ IM/src/test/java/com/wwwhynot3/im/ImApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wwwhynot3.im;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
